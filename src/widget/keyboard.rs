bcda782bafed45e46cf25254a42ab58377a645c5
Adrian Papari <adrian.papari@assaabloy.com>
2024-10-12 18:06:22 +0200

    adjust key width layout

--- src/widget/keyboard.rs
+++ src/widget/keyboard.rs
@@ -36,11 +36,11 @@ impl KeyboardLayout for AnsiKeyboardTklLayout {
     fn key_area(&self, key_code: KeyCode) -> Rect {
         let size = match key_code {
             KeyCode::Char(' ') => Size::new(SPACE_W, KEY_H),
-            KeyCode::Char('\\') => Size::new(8, KEY_H),
+            KeyCode::Char('\\') => Size::new(9, KEY_H),
             KeyCode::Tab => Size::new(TAB_W, KEY_H),
             KeyCode::CapsLock => Size::new(CAPSLOCK_W, KEY_H),
-            KeyCode::Backspace => Size::new(KEY_W * 2 - 1, KEY_H),
-            KeyCode::Enter => Size::new(12, KEY_H),
+            KeyCode::Backspace => Size::new(KEY_W * 2 - 2, KEY_H),
+            KeyCode::Enter => Size::new(13, KEY_H),
             KeyCode::Modifier(c) => match c {
                 ModifierKeyCode::LeftShift => Size::new(SHIFT_L_W, KEY_H),
                 ModifierKeyCode::RightShift => Size::new(SHIFT_R_W, KEY_H),
@@ -63,64 +63,66 @@ impl KeyboardLayout for AnsiKeyboardTklLayout {
     }
 
     fn key_position(&self, key_code: KeyCode) -> Position {
-        let offset = |row: &str, c: char| 1 + KEY_W * row.find(c).map_or(0, |i| i as u16);
+        let offset = |row: &str, c: char| (KEY_W - 1) * row.find(c).map_or(0, |i| i as u16);
 
         let fn_key_x = |n: u8| -> u16 {
             // F1 aligns with '2', but n is not zero-indexed, so we align it with '1'
-            let start = 1 + KEY_W;
+            let start = (KEY_W - 1);
 
             // group gap is ~3
             let group_gap = 2 * (((n as u16 - 1) / 4));
 
-            return start + group_gap + n as u16 * KEY_W;
+            return start + group_gap + n as u16 * (KEY_W - 1);
         };
 
+        let key_offset = |n: u16| -> u16 { n * (KEY_W - 1) };
+
         let (x, y) = match key_code {
-            KeyCode::Esc => (1, 0),
+            KeyCode::Esc => (0, 0),
             KeyCode::F(n) => (fn_key_x(n), 0),
             KeyCode::Char(c) if NUMBER_ROW.contains(c) => (offset(NUMBER_ROW, c), 3),
-            KeyCode::Char(c) if TOP_ROW.contains(c) => (TAB_W + offset(TOP_ROW, c), 5),
-            KeyCode::Char(c) if MIDDLE_ROW.contains(c) => (CAPSLOCK_W + offset(MIDDLE_ROW, c), 7),
-            KeyCode::Char(c) if BOTTOM_ROW.contains(c) => (SHIFT_L_W + offset(BOTTOM_ROW, c), 9),
-            KeyCode::Char(' ') => (1 + CTRL_L_W + SUPER_W + ALT_W, 11),
+            KeyCode::Char(c) if TOP_ROW.contains(c) => (TAB_W - 1 + offset(TOP_ROW, c), 5),
+            KeyCode::Char(c) if MIDDLE_ROW.contains(c) => (CAPSLOCK_W - 1 + offset(MIDDLE_ROW, c), 7),
+            KeyCode::Char(c) if BOTTOM_ROW.contains(c) => (SHIFT_L_W - 1 + offset(BOTTOM_ROW, c), 9),
+            KeyCode::Char(' ') => (CTRL_L_W + SUPER_W + ALT_W - 3, 11),
             KeyCode::Char(c) => (0, 0),
-            KeyCode::Backspace => (1 + 13 * KEY_W, 3),
-            KeyCode::Tab => (1, 5),
-            KeyCode::CapsLock => (1, 7),
-            KeyCode::Enter => (1 + CAPSLOCK_W + 11 * KEY_W, 7),
+            KeyCode::Backspace => (key_offset(NUMBER_ROW.len() as u16), 3),
+            KeyCode::Tab => (0, 5),
+            KeyCode::CapsLock => (0, 7),
+            KeyCode::Enter => (CAPSLOCK_W - 1 + key_offset(11), 7),
             KeyCode::Left => (NAV_KEY_X_START, 11),
-            KeyCode::Right => (NAV_KEY_X_START + KEY_W + KEY_W, 11),
-            KeyCode::Up => (NAV_KEY_X_START + KEY_W, 9),
-            KeyCode::Down => (NAV_KEY_X_START + KEY_W, 11),
-            KeyCode::Home => (NAV_KEY_X_START + KEY_W, 3),
-            KeyCode::End => (NAV_KEY_X_START + KEY_W, 5),
-            KeyCode::PageUp => (NAV_KEY_X_START + KEY_W * 2, 3),
-            KeyCode::PageDown => (NAV_KEY_X_START + KEY_W * 2, 5),
+            KeyCode::Right => (NAV_KEY_X_START + key_offset(2), 11),
+            KeyCode::Up => (NAV_KEY_X_START + key_offset(1), 9),
+            KeyCode::Down => (NAV_KEY_X_START + key_offset(1), 11),
+            KeyCode::Home => (NAV_KEY_X_START + key_offset(1), 3),
+            KeyCode::End => (NAV_KEY_X_START + key_offset(1), 5),
+            KeyCode::PageUp => (NAV_KEY_X_START + key_offset(2), 3),
+            KeyCode::PageDown => (NAV_KEY_X_START + key_offset(2), 5),
             KeyCode::BackTab => (0, 0),
             KeyCode::Delete => (NAV_KEY_X_START, 5),
             KeyCode::Insert => (NAV_KEY_X_START, 3),
             KeyCode::Null => (0, 0),
-            KeyCode::ScrollLock => (NAV_KEY_X_START + KEY_W, 0),
+            KeyCode::ScrollLock => (NAV_KEY_X_START + KEY_W - 1, 0),
             KeyCode::NumLock => (0, 0),
             KeyCode::PrintScreen => (NAV_KEY_X_START, 0),
-            KeyCode::Pause => (NAV_KEY_X_START + KEY_W * 2, 0),
-            KeyCode::Menu => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W, 11),
+            KeyCode::Pause => (NAV_KEY_X_START + key_offset(2), 0),
+            KeyCode::Menu => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W - 5, 11),
             KeyCode::KeypadBegin => (0, 0),
             KeyCode::Media(_) => (0, 0),
             KeyCode::Modifier(c) => match c {
-                ModifierKeyCode::LeftShift => (1, 9),
-                ModifierKeyCode::RightShift => (1 + SHIFT_L_W + KEY_W * BOTTOM_ROW.len() as u16, 9),
+                ModifierKeyCode::LeftShift => (0, 9),
+                ModifierKeyCode::RightShift => (SHIFT_L_W - 1 + key_offset(BOTTOM_ROW.len() as u16), 9),
 
-                ModifierKeyCode::LeftControl => (1, 11),
-                ModifierKeyCode::LeftSuper => (1 + CTRL_L_W, 11),
-                ModifierKeyCode::LeftHyper => (1 + CTRL_L_W, 11),
-                ModifierKeyCode::LeftMeta => (1 + CTRL_L_W, 11),
-                ModifierKeyCode::LeftAlt => (1 + CTRL_L_W + SUPER_W, 11),
-                ModifierKeyCode::RightAlt => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W, 11),
-                ModifierKeyCode::RightControl => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W + KEY_W, 11),
-                ModifierKeyCode::RightSuper => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W, 11),
-                ModifierKeyCode::RightHyper => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W, 11),
-                ModifierKeyCode::RightMeta => (1 + CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W, 11),
+                ModifierKeyCode::LeftControl => (0, 11),
+                ModifierKeyCode::LeftSuper => (CTRL_L_W - 1, 11),
+                ModifierKeyCode::LeftHyper => (CTRL_L_W - 1, 11),
+                ModifierKeyCode::LeftMeta => (CTRL_L_W - 1, 11),
+                ModifierKeyCode::LeftAlt => (CTRL_L_W + SUPER_W - 2, 11),
+                ModifierKeyCode::RightAlt => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W - 4, 11),
+                ModifierKeyCode::RightControl => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W + KEY_W - 7, 11),
+                ModifierKeyCode::RightSuper => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W - 6, 11),
+                ModifierKeyCode::RightHyper => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W - 6, 11),
+                ModifierKeyCode::RightMeta => (CTRL_L_W + SUPER_W + ALT_W + SPACE_W + ALT_W + MENU_W -6, 11),
                 ModifierKeyCode::IsoLevel3Shift => (0, 11), // ignore
                 ModifierKeyCode::IsoLevel5Shift => (0, 11), // ignore
             },
@@ -141,11 +143,11 @@ impl KeyboardLayout for AnsiKeyboardTklLayout {
             Char('`'), Char('1'), Char('2'), Char('3'), Char('4'), Char('5'), Char('6'), Char('7'),
             Char('8'), Char('9'), Char('0'), Char('-'), Char('='), Backspace,
 
-            // // top row
+            // top row
             Tab, Char('q'), Char('w'), Char('e'), Char('r'), Char('t'), Char('y'), Char('u'),
             Char('i'), Char('o'), Char('p'), Char('['), Char(']'), Char('\\'),
 
-            // // middle row
+            // middle row
             CapsLock, Char('a'), Char('s'), Char('d'), Char('f'), Char('g'), Char('h'), Char('j'),
             Char('k'), Char('l'), Char(';'), Char('\''), Enter,
 
@@ -156,13 +158,14 @@ impl KeyboardLayout for AnsiKeyboardTklLayout {
             // bottom row
             Modifier(LeftControl), Modifier(LeftSuper), Modifier(LeftAlt), Char(' '),
             Modifier(RightAlt), Menu, Modifier(RightSuper), Modifier(RightControl),
-            //
+
+            // nav keys
             PrintScreen, ScrollLock, Pause,
-            //
+
             Insert, Home, PageUp,
             Delete, End, PageDown,
-            //
-            // // cursor keys
+
+            // cursor keys
             Up, Left, Down, Right,
         ].into()
     }
@@ -227,7 +230,7 @@ impl KeyboardWidget {
 
     fn render_key(key_code: KeyCode, area: Rect, buf: &mut Buffer) {
         // draw key border, left
-        let (x, y) = (area.x.saturating_sub(1), area.y);
+        let (x, y) = (area.x, area.y);
         Self::draw_key_border('┌', &mut buf[(x, y + 0)]);
         Self::draw_key_border('│', &mut buf[(x, y + 1)]);
         Self::draw_key_border('└', &mut buf[(x, y + 2)]);
@@ -297,10 +300,10 @@ impl KeyboardWidget {
             },
         };
 
-        let alignment = if key_string.char_indices().count() != 3 { Alignment::Center } else { Alignment::Left };
+        let alignment = if key_string.char_indices().count() > 1 { Alignment::Left } else { Alignment::Center };
         Text::from(Span::from(key_string))
             .alignment(alignment)
-            .render(area.offset(Offset { x: 0, y: 1 }), buf);
+            .render(area.offset(Offset { x: 1, y: 1 }), buf);
     }
 }
 
@@ -338,9 +341,9 @@ impl Widget for Key {
     }
 }
 
-const NAV_KEY_X_START: u16 = 78;
+const NAV_KEY_X_START: u16 = 79;
 
-const KEY_W: u16 = 5; // includes | delimited
+const KEY_W: u16 = 6; // includes | delimited
 const KEY_H: u16 = 3;
 
 const NUMBER_ROW: &str = "`1234567890-=";
@@ -348,14 +351,14 @@ const TOP_ROW: &str = "qwertyuiop[]\\";
 const MIDDLE_ROW: &str = "asdfghjkl;'";
 const BOTTOM_ROW: &str = "zxcvbnm,./";
 
-const TAB_W: u16 = 6;
-const CAPSLOCK_W: u16 = 7;
-const SHIFT_L_W: u16 = 9;
-const SHIFT_R_W: u16 = 15;
-const CTRL_L_W: u16 = 6;
-const CTRL_R_W: u16 = 9;
-const ALT_W: u16 = 7;
-const SPACE_W: u16 = 30;
+const TAB_W: u16 = 7;
+const CAPSLOCK_W: u16 = 8;
+const SHIFT_L_W: u16 = 10;
+const SHIFT_R_W: u16 = 16;
+const CTRL_L_W: u16 = 7;
+const CTRL_R_W: u16 = 10;
+const ALT_W: u16 = 8;
+const SPACE_W: u16 = 31;
 
-const SUPER_W: u16 = 5;
-const MENU_W: u16 = 5;
+const SUPER_W: u16 = 6;
+const MENU_W: u16 = 6;
